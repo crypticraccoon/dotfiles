ef7212d0b05acd486f34181d29930fef021429ab
crypticraccoon <maahase@outlook.com>
2025-05-28 22:45:31 -0400

    update

--- confs/eww/launch.sh
+++ confs/eww/launch.sh
@@ -4,12 +4,14 @@ args=$1
 ewwLoaded=$(pidof eww)
 
 if [[ ! $ewwLoaded ]];then
-	 eww daemon > /dev/null 2>&1
+	 #eww daemon > /dev/null 2>&1
+	 eww daemon
 fi
 
 while true; do
 	 if [[ $(eww ping) == "pong" ]];then
-			eww open $args > /dev/null 2>&1
+			eww open $args
+			#eww open $args > /dev/null 2>&1
 			break
 	 fi
 	 notify-send "Failed to start eww daemon"
