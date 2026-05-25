a2304a238b8905a264944ef65baf505f4f3a8470
crypticraccoon <maahase@outlook.com>
2026-05-25 16:05:08 -0400

    added cmd to format file

--- confs/nvim/lua/config/lsp/formatter.lua
+++ confs/nvim/lua/config/lsp/formatter.lua
@@ -17,19 +17,30 @@ local _ = {
 
 require("conform").setup({
 	formatters_by_ft = {
+		python = { "autopep8", lsp_format = "fallback" },
 		bash = { "shfmt" },
 		yaml = { "yq", lsp_format = "fallback" },
-		rust = { "rustfmt", lsp_formaaat = "fallback" },
+		rust = { "rustfmt", lsp_format = "fallback" },
 		lua = { "stylua" },
 		json = { "jq" },
 		go = { "gofmt" },
-		python = { "autopep8" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "eslint_d", "prettier" },
 		typescriptreact = { "eslint_d", "prettier" },
 	},
 })
 
---vim.keymap.set("n", "<leader>f", function()
---require("conform").format({ formatters = { "yq" } })
---end, { desc = "Toggle auto format" })
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
+
+vim.keymap.set('n', '<space>f', ":Format<CR>" )
